7662e1151b0e01dc555f57ef7f7c1aa3fdb2a9de
jst1ws <jernej.strazisar@scania.com>
2024-02-15 11:57:46 +0100

    Install Tegel angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TegelModule } from '@scania/tegel-angular';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TegelModule,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
