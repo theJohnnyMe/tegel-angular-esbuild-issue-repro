ca38434cd538f69f1cfe5565f42f1fe2c4804519
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2024-05-21 16:34:19 +0200

    fix: testing angular 17

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { defineCustomElements } from '@scania/tegel/loader';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+defineCustomElements(window);
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
